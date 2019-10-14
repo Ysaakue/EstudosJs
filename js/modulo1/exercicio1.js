9e8b2f7c71c977aa9f5143356709a06f155cccb3
João Victor <40237577+Juaozim@users.noreply.github.com>
2019-10-14 13:07:06 -0300

    comentarios

--- js/modulo1/exercicio1.js
+++ js/modulo1/exercicio1.js
@@ -1,8 +1,9 @@
 // require('../index');
 
 function primeira01() {
+   // selecionandoo elemento com id 'um' 
    var target = document.querySelector('#um');
-
+   // montando o objeto endereço
    var endereco = {
       rua: "Rua dos pinheiros",
       numero: 1293,
@@ -10,9 +11,9 @@ function primeira01() {
       cidade: "São Paulo",
       uf: "SP"
       };
-
+   //montando string concatenando com os dados do objeto endereço
    var resultado = 'O usuário mora em '+ endereco.cidade + ' / ' + endereco.uf + ', no bairro' + endereco.bairro +', na rua "'+ endereco.rua +'" com nº ' + endereco.numero;
-
+   //colocando a string resultado como valor do campo selecionado pelo id no inicio
    target.value = resultado;
 
    newHeight(target);
